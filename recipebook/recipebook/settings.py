555ac9ddd469c5f9a0d8b080e465d7d1ca5283a0
moira-b <moiraysabelle7@gmail.com>
2025-02-27 17:32:12 +0800

    Initial commit for new branch

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q63(l1n)03!k1z^(@2xcn=w1s8liql#+z50p9a43qv18!7&&#='
+# SECURITY WARNING: keep the secret key used in production secret!x
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
