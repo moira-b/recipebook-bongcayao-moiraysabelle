b03c67744071bacbc1ca63989e2fc8ccf3ce4661
moira-b <moiraysabelle7@gmail.com>
2025-02-27 18:34:57 +0800

    Created models in ledger/models.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('ingredient',args=[str(self.name)])
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('recipe',args=[str(self.name)])
+
+class RecipeIngredient(models.Model):
+    quantity = models.DecimalField(decimal_places=2, max_digits = 10) # real-life recipes will generally not need to exceed these parameters
+    ingredientType = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
+    inRecipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
