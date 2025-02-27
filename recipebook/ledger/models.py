6c2fae0560eb471f046ac9c04e28ab1efe1c58de
moira-b <moiraysabelle7@gmail.com>
2025-02-27 23:07:33 +0800

    Fixes to model implementation, admin implementation

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -6,22 +6,20 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
     def get_absolute_url(self):
-        return reverse('ingredient',args=[str(self.name)])
+        return reverse('ledger:ingredient',args=[str(self.name)])
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
         return self.name
     def get_absolute_url(self):
-        return reverse('recipe',args=[str(self.name)])
+        return reverse('ledger:recipe',args=[str(self.pk)])
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
     ingredientType = models.ForeignKey(Ingredient, 
                                        on_delete=models.CASCADE, 
-                                       related_name='recipe')
+                                       related_name='recipe',)
     inRecipe = models.ForeignKey(Recipe, 
                                  on_delete=models.CASCADE, 
-                                 related_name='ingredients')
-    def __str__(self):
-        return(f"{self.ingredientType.name} in {self.inRecipe.name}")
+                                 related_name='ingredients',)
--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
-# from .views import recipesList, recipe1, recipe2
 from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    path('recipe-list', RecipeListView.as_view(), name='recipe-list'),
-    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe-details'),
-    path('', admin, name='admin')
-    # path('', index, name='index'),
-    # path('', recipesList, name='recipe-list'),
-    # path('recipes/list', recipesList, name='recipe-list'),
-    # path('recipe/1', recipe1, name='recipe-1'),
-    # path('recipe/2', recipe2, name='recipe-2'),
+    path('',RecipeListView.as_view(),name='recipe-list'),
+    path('recipes/list', RecipeListView.as_view(), name='recipe-list'),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe'),
 ]
 
 app_name = 'ledger'
