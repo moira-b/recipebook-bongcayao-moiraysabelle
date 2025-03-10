ac0244c642a110ad5912bcd968272fccb6054828
moira-b <moiraysabelle7@gmail.com>
2025-03-10 20:25:06 +0800

    Fixed issues with password reset and user authentication status

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,25 +1,34 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return str(self.name)
+
     def get_absolute_url(self):
-        return reverse('ledger:ingredient',args=[str(self.name)])
+        return reverse('ledger:ingredient', args=[str(self.name)])
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return str(self.name)
+
     def get_absolute_url(self):
-        return reverse('ledger:recipe',args=[str(self.pk)])
-    
+        return reverse('ledger:recipe', args=[str(self.pk)])
+
     author = models.CharField(max_length=100, default="Anonymous")
     created_on = models.DateTimeField(auto_now_add=True, null=True)
     updated_on = models.DateTimeField(auto_now=True, null=True)
 
+
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
-    ingredientType = models.ForeignKey(Ingredient,on_delete=models.CASCADE,related_name='recipe',)
-    inRecipe = models.ForeignKey(Recipe,on_delete=models.CASCADE,related_name='ingredients',)
+    ingredient_Type = models.ForeignKey(
+        Ingredient, on_delete=models.CASCADE, related_name='recipe',)
+    in_Recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='ingredients',)
--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    path('',RecipeListView.as_view(),name='recipe-list'),
     path('recipes/list', RecipeListView.as_view(), name='recipe-list'),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe'),
 ]
